fd5143ec53881c755672f3cec7d6457642ca6d3b
luizgabriel9997 <https://luizgabriel9997@bitbucket.org>
2020-09-14 21:59:18 -0300

    terminado menu e iniciado list de plantas - falta conectar com o firebase

---
+++ ipot_integrador/lib/lista_plantas.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ListaPlantas extends StatefulWidget {
+  @override
+  _ListaPlantasState createState() => _ListaPlantasState();
+}
+
+class _ListaPlantasState extends State<ListaPlantas> {
+  @override
+  Widget build(BuildContext context) {
+    return new Center();
+  }
+}
--- ipot_integrador/lib/main.dart
+++ ipot_integrador/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ipot_integrador/header.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        visualDensity: VisualDensity.compact,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -26,20 +27,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: Header(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -47,17 +38,8 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
